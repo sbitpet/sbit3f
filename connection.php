18cd53d70fef6f6e297cab3eccb9c4dd11fecb4a
sbitpet <133607634+sbitpet@users.noreply.github.com>
2023-05-15 06:26:40 +0800

    Update and rename conn.php to connection.php

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+$conn = mysqli_connect("sbit3f-gym-2.ctwnycxphco9.ap-southeast-1.rds.amazonaws.com","admin","sbit3fruben","sbit3f");
+
+
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  exit();
+}
+
+?>
